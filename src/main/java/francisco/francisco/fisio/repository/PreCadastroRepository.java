49d6f9ea25c4443e6efbe0b875c13f8ec3f0d2dc
marcelo <marcelo1195.mf@gmail.com>
2024-05-10 18:45:01 -0300

    repositorios montados

---
+++ src/main/java/francisco/francisco/fisio/repository/PreCadastroRepository.java
@@ -0,0 +1,7 @@
+package francisco.francisco.fisio.repository;
+
+import francisco.francisco.fisio.model.PreCadastro;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PreCadastroRepository extends JpaRepository <PreCadastro, Long> {
+}

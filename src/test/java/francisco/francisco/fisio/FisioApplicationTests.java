d1497e997fbdf36b3b4a201255f50d9087161a75
marcelo <marcelo1195.mf@gmail.com>
2024-05-10 15:28:29 -0300

    Initial commit

---
+++ src/test/java/francisco/francisco/fisio/FisioApplicationTests.java
@@ -0,0 +1,13 @@
+package francisco.francisco.fisio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FisioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
